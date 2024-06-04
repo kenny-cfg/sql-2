ecb0907091ae1238526a37d22f46db540bab1a8d
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-06-04 19:56:31 +0100

    Slide 10 with orders table

--- slide.10.sql
+++ slide.10.sql
@@ -6,13 +6,19 @@ use customers;
 
 create table customers (
   customer_id integer primary key,
-  customer_name varchar(50),
-  customer_address varchar(200),
-  customer_telephone_number varchar(20)
+  customer_name varchar(50) not null,
+  customer_address varchar(200) not null,
+  customer_telephone_number varchar(20) not null
 );
 
 create table products (
   product_id varchar(10) primary key,
-  product_name varchar(200),
-  unit_cost decimal
+  product_name varchar(200) not null,
+  unit_cost decimal not null
 );
+
+create table orders (
+  order_id varchar(10) primary key,
+  customer_id integer not null,
+  foreign key (customer_id) references customers(customer_id)
+)
